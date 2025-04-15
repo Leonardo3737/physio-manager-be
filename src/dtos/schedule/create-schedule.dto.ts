fdb0929fed56c7490a9bb8fe8c0ef823954ac754
Kaik <119710298+KaikV@users.noreply.github.com>
2025-04-14 23:50:49 -0400

    Update create-schedule.dto.ts

--- src/dtos/schedule/create-schedule.dto.ts
+++ src/dtos/schedule/create-schedule.dto.ts
@@ -1,9 +1,7 @@
-// src/dtos/schedule/create-schedule.dto.ts
 import { z } from 'zod'
 import { ScheduleSchema } from './schedule.schema'
 import { DTO } from '../dto'
 
-// Remove campos que não devem ser recebidos na criação
 export const CreateScheduleSchema = ScheduleSchema.omit({
   id: true,
   createdAt: true,
